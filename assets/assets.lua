0840877900c59784c019bcdcdddc229995d5f336
EngineerSmith <pasmith.bus@gmail.com>
2023-03-01 23:34:50 +0000

    Added basic audio

--- assets/assets.lua
+++ assets/assets.lua
@@ -3,4 +3,8 @@ return {
   ["font.bold"] = "assets/fonts/OpenDyslexic-Bold.otf",
   ["font.italic"] = "assets/fonts/OpenDyslexic-Italic.otf",
   ["font.bolditalic"] = "assets/fonts/OpenDyslexic-Bold-Italic.otf",
+
+  ["audio.ui.error"] = "assets/audio/CGM3_Error_Button_05_3.wav",
+  ["audio.ui.button"] = "assets/audio/FA_Cute_Jump_3.wav",
+  ["audio.ui.tick"] = "assets/audio/LQ_Ticking_1_2.wav",
 }
