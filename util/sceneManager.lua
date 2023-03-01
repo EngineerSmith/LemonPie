46fecdba36d278ef054e510e99516370320f46bc
EngineerSmith <pasmith.bus@gmail.com>
2023-03-01 02:38:43 +0000

    Added my sceneManager

---
+++ util/sceneManager.lua
@@ -0,0 +1,73 @@
+local sceneManager = {
+  currentScene = nil,
+  nilFunc = function() end,
+  sceneHandlers = {
+    -- GAME LOOP
+    "load",
+    "unload",
+    "update",
+    "updateNetwork",
+    "draw",
+    "quit",
+    -- WINDOW
+    "focus",
+    "resize",
+    "visable",
+    "displayrotated",
+    "filedropped",
+    "directorydropped",
+    -- TOUCH INPUT
+    "touchpressed",
+    "touchmoved",
+    "touchreleased",
+    -- MOUSE INPUT
+    "mousepressed",
+    "mousemoved",
+    "mousereleased",
+    "mousefocus",
+    "wheelmoved",
+    -- KEY INPUT,
+    "keypressed",
+    "keyreleased",
+    "textinput",
+    "textedited",
+    -- JOYSTICK/GAMEPAD INPUT
+    "joystickhat",
+    "joystickaxis",
+    "joystickpressed",
+    "joystickreleased",
+    "joystickadded",
+    "joystickremoved",
+    "gamepadpressed",
+    "gamepadreleased",
+    "gamepadaxis",
+    -- ERROR
+    "threaderror",
+    "lowmemory",
+  },
+}
+
+local love = love
+
+sceneManager.changeScene = function(sceneRequire, ...)
+  local scene = require(sceneRequire)
+  if sceneManager.currentScene then
+    love.unload()
+  end
+  for _, v in ipairs(sceneManager.sceneHandlers) do
+    love[v] = scene[v] or sceneManager.nilFunc
+  end
+  if love["quit"] ~= sceneManager.nilFunc then
+    love["quit"] = sceneManager.quit
+  end
+  sceneManager.currentScene = scene
+  collectgarbage("collect")
+  collectgarbage("collect")
+  love.load(...)
+end
+
+sceneManager.quit = function()
+  return sceneManager.currentScene.quit()
+end
+
+return sceneManager
