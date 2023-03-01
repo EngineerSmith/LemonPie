8486caa93debd22dcb66c712196126a3bf2f3cc8
EngineerSmith <pasmith.bus@gmail.com>
2023-03-01 22:00:19 +0000

    Added OpenDyslexic font

---
+++ assets/assets.lua
@@ -0,0 +1,6 @@
+return {
+  ["font.regular"] = "assets/fonts/OpenDyslexic-Regular.otf",
+  ["font.bold"] = "assets/fonts/OpenDyslexic-Bold.otf",
+  ["font.italic"] = "assets/fonts/OpenDyslexic-Italic.otf",
+  ["font.bolditalic"] = "assets/fonts/OpenDyslexic-Bold-Italic.otf",
+}
